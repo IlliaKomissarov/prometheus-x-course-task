300876faa481f997d40a6486d23a18335bfc6a94
Illia Komissarov <iliakomissarow99@gmail.com>
2024-02-17 20:07:22 +0200

    Create localStorage.jsx

---
+++ src/services/localStorage.jsx
@@ -0,0 +1,31 @@
+const LS_KEYS = {
+  USERNAME: 'username',
+  CART: 'cart',
+  LOGGEDIN: 'loggedIn',
+};
+
+class LocalStorageService {
+  static get(key) {
+    const value = window.localStorage.getItem(key);
+
+    try {
+      return JSON.parse(value);
+    } catch {
+      return value;
+    }
+  }
+
+  static set(key, value) {
+    return window.localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static remove(key) {
+    return window.localStorage.removeItem(key);
+  }
+
+  static removeAll() {
+    return window.localStorage.clear();
+  }
+}
+
+export { LocalStorageService, LS_KEYS };
